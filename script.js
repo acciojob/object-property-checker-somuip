862f47d51c11a8e87d851a5bfe08b6da26143820
somuip <117620797+somuip@users.noreply.github.com>
2023-07-15 11:02:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,12 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
-}
+   const allKeys = Object.keys(sampleObject);
+   for (let i = 0; i < allKeys.length; i++) {
+       if(key === allKeys[i]) return true // Prints each key in the object
+   }
+	return false;
+};
 
 // Do not change the code below
 const key = prompt("Enter Key.");
